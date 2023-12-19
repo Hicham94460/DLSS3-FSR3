1a18c6d6a14d450facc1a9048032b94cb18d8c45
Hicham94460 <140126094+Hicham94460@users.noreply.github.com>
2023-12-19 12:50:15 +0100

    Add files via upload

---
+++ Make-Release.ps1
@@ -0,0 +1,14 @@
+# Set up powershell equivalent of vcvarsall.bat
+$vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationpath
+
+Import-Module (Get-ChildItem $vsPath -Recurse -File -Filter Microsoft.VisualStudio.DevShell.dll).FullName
+Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -DevCmdArguments '-arch=x64'
+
+# Then build with VS
+& cmake --preset final-dtwrapper
+& cmake --build --preset final-dtwrapper-release
+& cpack --preset final-dtwrapper
+
+& cmake --preset final-nvngxwrapper
+& cmake --build --preset final-nvngxwrapper-release
+& cpack --preset final-nvngxwrapper
---
+++ source/maindll/dllmain.cpp
@@ -0,0 +1,53 @@
+#include <Windows.h>
+#include <charconv>
+
+BOOL WINAPI RawDllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+		// Start vsjitdebugger.exe if a debugger isn't already attached. VS_DEBUGGER_REQUEST determines the
+		// command line and VS_DEBUGGER_PROC is used to hide the CreateProcessA IAT entry.
+		if (char cmd[256] = {}, proc[256] = {}; !IsDebuggerPresent() &&
+												GetEnvironmentVariableA("VS_DEBUGGER_REQUEST", cmd, ARRAYSIZE(cmd)) > 0 &&
+												GetEnvironmentVariableA("VS_DEBUGGER_PROC", proc, ARRAYSIZE(proc)) > 0)
+		{
+			std::to_chars(cmd + strlen(cmd), std::end(cmd), GetCurrentProcessId());
+			auto moduleName = proc;
+			auto importName = strchr(proc, '!') + 1;
+			importName[-1] = '\0';
+
+			PROCESS_INFORMATION pi = {};
+			STARTUPINFOA si = {};
+			si.cb = sizeof(si);
+
+			auto c = reinterpret_cast<decltype(&CreateProcessA)>(GetProcAddress(GetModuleHandleA(moduleName), importName));
+			c(nullptr, cmd, nullptr, nullptr, false, 0, nullptr, nullptr, &si, &pi);
+
+			WaitForSingleObject(pi.hProcess, INFINITE);
+			CloseHandle(pi.hProcess);
+			CloseHandle(pi.hThread);
+		}
+	}
+
+	return TRUE;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+		OutputDebugStringW(L"DEBUG: Built with commit ID " BUILD_GIT_COMMIT_HASH "\n");
+
+		const wchar_t *message = L"dlssg-to-fsr3 is loaded. AMD FSR 3 Frame Generation will replace Nvidia DLSS-G Frame Generation."
+								 L"\n\nPlease note this is experimental software and is subject to bugs and/or crashes. This does "
+								 L"NOT represent a native implementation of AMD's FSR 3.\n\nReport issues at "
+								 L"https://github.com/Nukem9/dlssg-to-fsr3\n\nWARNING: DO NOT USE IN MULTIPLAYER GAMES.";
+
+		MessageBoxW(nullptr, message, L"dlssg-to-fsr3", MB_OK);
+		return TRUE;
+	}
+
+	return TRUE;
+}
+
+extern "C" extern decltype(&RawDllMain) const _pRawDllMain = RawDllMain;
